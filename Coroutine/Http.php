7a0322d8b3209013ca317283b3878b3ce94d0a1d
向林 <as2252258@163.com>
2022-02-14 14:46:41 +0800

    modify plugin name

--- Coroutine/Http.php
+++ Coroutine/Http.php
@@ -164,16 +164,14 @@ class Http extends Component
 						}
 					}
 
-					if ($response->isWritable()) {
-						while (($data = $response->recv()) instanceof Frame) {
-							try {
-								if ($data->opcode == WEBSOCKET_OPCODE_PING || $data->opcode == WEBSOCKET_OPCODE_PONG) {
-									continue;
-								}
-								call_user_func($message, $data);
-							} catch (\Throwable $throwable) {
-								$this->logger()->error($throwable->getMessage());
+					while (($data = $response->recv()) instanceof Frame) {
+						try {
+							if ($data->opcode == WEBSOCKET_OPCODE_PING || $data->opcode == WEBSOCKET_OPCODE_PONG) {
+								continue;
 							}
+							call_user_func($message, $data);
+						} catch (\Throwable $throwable) {
+							$this->logger()->error($throwable->getMessage());
 						}
 					}
 					call_user_func($close, $response->fd);
