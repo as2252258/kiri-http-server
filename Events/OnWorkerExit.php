d8005f21f994be80b416c1bac950f4ad8cc23b7a
as2252258 <as2252258@163.com>
2022-04-10 15:34:12 +0800

    modify mysql result

--- Events/OnWorkerExit.php
+++ Events/OnWorkerExit.php
@@ -11,11 +11,11 @@ class OnWorkerExit
 {
 
 
-	/**
-	 * @param Server $server
-	 * @param int $workerId
-	 */
-	public function __construct(public Server $server, public int $workerId)
+    /**
+     * @param Server|null $server
+     * @param int $workerId
+     */
+	public function __construct(public ?Server $server, public int $workerId)
 	{
 	}
 
