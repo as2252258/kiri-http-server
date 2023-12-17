375885fdd6be3567be3429fa51f3dbf7e4abc1c6
whwyy <as2252258@163.com>
2023-12-18 03:30:11 +0800

    eee

--- HotReload.php
+++ HotReload.php
@@ -306,7 +306,7 @@ class HotReload extends BaseProcess
      */
     public function trigger_reload(): void
     {
-        $this->logger->error('Wait trigger server Reload');
+        $this->logger->failure('Wait trigger server Reload');
         di(ServerInterface::class)->reload(false);
     }
 
